24265a35ed8dd93f09a49fe001bcc87c216c44aa
Bas Wildemann <bas@wildemann.nl>
2014-11-13 15:12:04 +0000

    parameter validation

--- manifests/init.pp
+++ manifests/init.pp
@@ -38,11 +38,41 @@
 class beaver(
 
   $ensure = $beaver::params::ensure,
+  $status = $beaver::params::status,
+  $format = $beaver::params::format,
+  $version       = false,
+  $format        = 'json',
+  $respawn_delay = 3,
+  $max_failure   = 7,
+  $hostname      = $::fqdn,
+  $transport     = 'redis'
 
 ) inherits beaver::params {
   
   if ! ($ensure in [ 'present', 'absent' ]) {
     fail("\"${ensure}\" invalid value for parameter 'ensure'. Valid parameters are: 'present absent'")
+  }
+
+  if ! ($status in [ 'enabled', 'disabled', 'running', 'unmanaged' ]) {
+    fail("\"${status}\" invalid value for parameter 'status'. Valid parameters are: 'enabled disabled running unmanaged'")
+  }
+
+  if ! ($format in [ 'json', 'msgpack', 'string', 'raw' ]) {
+    fail("\"${format}\" invalid value for parameter 'format'. Valid parameters are: 'json msgpak string raw")
+  }
+
+  if ! is_numeric($respawn_delay) {
+    fail("\"${respawn_delay}\" is not a valid respawn_delay value")
+  }
+
+  if ! is_numeric($max_failure) {
+    fail("\"${max_failure}\" invalid value for max_failure parameter")
+  }
+
+  validate_string($hostname)
+
+  if ! ($transport in [ 'redis', 'rabbitmq', 'zmq', 'udp', 'mqtt', 'sqs' ]) {
+    fail("\"${transport}\"  invalid value for parameter 'transport'. Valid parameters are: 'redis rabbitmq zmq udp mqtt sqs'")
   }
 
 }
