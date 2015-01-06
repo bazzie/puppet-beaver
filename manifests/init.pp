21b770eaf4f1e58c94278747b88af9f3ee00130f
bas <bas@192.168.1.90>
2015-01-06 16:17:25 +0100

    Removed checks

--- manifests/init.pp
+++ manifests/init.pp
@@ -49,32 +49,6 @@ class beaver(
 
 ) inherits beaver::params {
   
-  if ! ($ensure in [ 'present', 'absent' ]) {
-    fail("\"${ensure}\" invalid value for parameter 'ensure'. Valid parameters are: 'present absent'")
-  }
-
-  if ! ($status in [ 'enabled', 'disabled', 'running', 'unmanaged' ]) {
-    fail("\"${status}\" invalid value for parameter 'status'. Valid parameters are: 'enabled disabled running unmanaged'")
-  }
-
-  if ! ($format in [ 'json', 'msgpack', 'string', 'raw' ]) {
-    fail("\"${format}\" invalid value for parameter 'format'. Valid parameters are: 'json msgpak string raw")
-  }
-
-  if ! is_numeric($respawn_delay) {
-    fail("\"${respawn_delay}\" is not a valid respawn_delay value")
-  }
-
-  if ! is_numeric($max_failure) {
-    fail("\"${max_failure}\" invalid value for max_failure parameter")
-  }
-
-  validate_string($hostname)
-
-  if ! ($transport in [ 'redis', 'rabbitmq', 'zmq', 'udp', 'mqtt', 'sqs' ]) {
-    fail("\"${transport}\"  invalid value for parameter 'transport'. Valid parameters are: 'redis rabbitmq zmq udp mqtt sqs'")
-  }
-
   class { 'beaver::install': } ->
   class { 'beaver::config': } ~>
   class { 'beaver::service': } ->
