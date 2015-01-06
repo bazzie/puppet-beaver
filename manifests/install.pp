61aeebd71b77b12a694c14adad1e73acc135c3fa
bas <bas@192.168.1.90>
2015-01-06 16:35:21 +0100

    create init script and directories

--- manifests/install.pp
+++ manifests/install.pp
@@ -10,5 +10,31 @@ class beaver::install {
     notify => Class['beaver::service'],
   }
 
+  file { '/etc/init.d/beaver':
+    ensure => file,
+    mode => '0555',
+    owner => 'root',
+    group => 'root',
+    content => template('beaver/beaver.init.erb'),
+  }
+
+  file { '/etc/beaver':
+    ensure => 'directory',
+    mode => '0555',
+    owner => 'root',
+    group => 'root',
+  }
+
+  file { '/etc/beaver/conf.d':
+    ensure => 'directory',
+    mode => '0555',
+    owner => 'root',
+    group => 'root',
+    purge => true,
+    force => true,
+    recurse => true,
+    notify => Class['beaver::service'],
+  }
+
 
 }
