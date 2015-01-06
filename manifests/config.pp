8f377d4cb34550ad8838f2809e3047718f70424d
bas <bas@192.168.1.90>
2015-01-06 16:13:45 +0100

    beaver::config

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,25 +1,12 @@
 class beaver::config {
 
-  file { '/etc/beaver/':
-    ensure => directory,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644'
-  }
-
-  file_fragment { 'header':
-    tag     => "beaver_config_${::fqdn}",
-    content => "[beaver]\n${beaver::config}\n",
-    order   => 10
-  }
-
-  file_concat { '/etc/beaver/beaver.conf':
-    tag     => "beaver_config_${::fqdn}",
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    notify  => Service['beaver'],
-    require => File['/etc/beaver/']
+  file { '/etc/beaver.conf':
+    ensure => 'file',
+    owner => 'root',
+    group => 'root',
+    mode => '0444',
+    content => template('beaver/beaver.conf.erb'),
+    notify => Class['beaver::service']
   }
 
 }
